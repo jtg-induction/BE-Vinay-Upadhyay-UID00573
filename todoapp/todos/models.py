efa8f21d5973f51a12f1f4031da46c3f9f2c18f0
vinay.upadhyay <vinay.upadhyay@joshtechnologygroup.com>
2025-02-05 20:19:46 +0530

    Minor db changes

--- todoapp/todos/models.py
+++ todoapp/todos/models.py
@@ -15,7 +15,7 @@ class Todo(models.Model):
     name = models.CharField(_("Name Of Task"), max_length=1000)
     done = models.BooleanField(_("Completed"), default=False)
     date_created = models.DateTimeField(_("Date Of Task Created"), default=now)
-    date_completed = models.DateTimeField(_("Date Of Completion"), blank=True)
+    date_completed = models.DateTimeField(_("Date Of Completion"), blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if len(self.name) == 0:

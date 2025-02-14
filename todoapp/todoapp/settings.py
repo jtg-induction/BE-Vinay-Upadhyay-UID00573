3a8cf758586b7d8036b5126281f884625e58c686
vinay.upadhyay <vinay.upadhyay@joshtechnologygroup.com>
2025-02-14 12:46:26 +0530

    squashing migration

--- todoapp/todoapp/settings.py
+++ todoapp/todoapp/settings.py
@@ -88,9 +88,9 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 DATABASES = {
     u'default': {
         u'ENGINE': u'django.db.backends.postgresql',
-        u'NAME': u'',                      # Add database name.
-        u'USER': u'postgres',              # Add psql user name. For default use postgres.
-        u'PASSWORD': u'',                  # Add user password if exists.
+        u'NAME': u'mydatabase',                      # Add database name.
+        u'USER': u'vinay',              # Add psql user name. For default use postgres.
+        u'PASSWORD': u'Vinay@010104',                  # Add user password if exists.
         u'HOST': u'',                      # Set to empty string for localhost..
         u'PORT': u'5432',                  # Psql service running port.
     },

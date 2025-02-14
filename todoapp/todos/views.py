7056508f172ed28a9a2478841b0b3601299bf3b0
vinay.upadhyay <vinay.upadhyay@joshtechnologygroup.com>
2025-02-14 17:52:02 +0530

    review fixes

--- todoapp/todos/views.py
+++ todoapp/todos/views.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers, status
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Todo
-from .serializers import TodoApiViewSetCreateSerializer, TodoViewSetSerialzer
+from todos.models import Todo
+from todos.serializers import TodoApiViewSetCreateSerializer, TodoViewSetSerialzer
 
 
 class TodoAPIViewSet(ModelViewSet):
@@ -26,18 +27,21 @@ class TodoAPIViewSet(ModelViewSet):
           }
         ]
     """
+    queryset = Todo.objects.all()
+    permission_classes = (AllowAny,)
 
     def get_serializer_class(self):
-        todo_api_viewset_serializer = TodoViewSetSerialzer
+        serializer = TodoViewSetSerialzer
         if self.action == 'create':
-             todo_api_viewset_serializer = TodoApiViewSetCreateSerializer
+             serializer = TodoApiViewSetCreateSerializer
 
-        return todo_api_viewset_serializer
+        return serializer
     
     def get_queryset(self):
-        user_id = self.request.query_params.get('user_id', None)
-        todo_objects = Todo.objects.all()
+        queryset = super().get_queryset()
+        import pdb; pdb.set_trace()
+        user_id = self.request.data.get('user_id', None)
         if user_id is not None:
-            todo_objects =  Todo.objects.filter(user__id=user_id)
-        
-        return todo_objects
+            queryset =  queryset.filter(user__id=user_id)
+
+        return queryset

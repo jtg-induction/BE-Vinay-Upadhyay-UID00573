9361056b205adf4e2ed87382c6a391a4a64e8536
vinay.upadhyay <vinay.upadhyay@joshtechnologygroup.com>
2025-02-15 16:00:39 +0530

    exercise 9 and 10 completed

--- todoapp/users/serializers.py
+++ todoapp/users/serializers.py
@@ -1,15 +1,27 @@
 
 # Add your serializers
 from rest_framework import serializers
-from .models import CustomUser
+
+from users.models import CustomUser
+
 
 class UserBaseSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = CustomUser
         fields = ['first_name', 'last_name', 'email']
 
 
 class UserSerializer(UserBaseSerializer):
+
     class Meta:
         model = CustomUser
         fields = UserBaseSerializer.Meta.fields + ['id']
+
+
+class UserRegistrationSerializer(UserBaseSerializer):
+    password = serializers.CharField(write_only = True)
+    
+    class Meta:
+        model = CustomUser
+        fields = UserBaseSerializer.Meta.fields + ['password', 'date_joined']
--- todoapp/users/views.py
+++ todoapp/users/views.py
@@ -1,4 +1,12 @@
-class UserRegistrationAPIView():
+from django.contrib.auth import authenticate
+from rest_framework import generics, permissions, response, views
+from rest_framework.authtoken.models import Token
+
+from users.models import CustomUser
+from users.serializers import UserRegistrationSerializer
+
+
+class UserRegistrationAPIView(generics.CreateAPIView, generics.GenericAPIView):
     """
         success response format
          {
@@ -9,12 +17,38 @@ class UserRegistrationAPIView():
            "token"
          }
     """
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserRegistrationSerializer
 
+    def post(self, request, *args, **kwargs):
+        serialized_data = self.get_serializer(data=request.data)
 
-class UserLoginAPIView():
+        if not serialized_data.is_valid():
+            return response.Response(serialized_data.errors)
+        
+        data = serialized_data.validated_data
+        user = CustomUser.objects.create_user(**data)
+        token, created = Token.objects.get_or_create(user=user)
+        data["token"] = token.key
+
+        return  response.Response(data)
+
+             
+class UserLoginAPIView(views.APIView):
     """
         success response format
          {
            auth_token: ""
          }
     """
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        user = authenticate(username=request.data['email'], password=request.data['password'])
+        respons = response.Response({'error' : 'Invalid credentials'}, status=401)
+
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            respons = response.Response({'token': token.key})
+      
+        return respons

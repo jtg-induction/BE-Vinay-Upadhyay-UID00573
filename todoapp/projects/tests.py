325775af1de9a46b1d3c9b06214c9a235ef48540
vinay.upadhyay <vinay.upadhyay@joshtechnologygroup.com>
2025-02-17 11:53:34 +0530

    exercise14 completed

--- todoapp/projects/tests.py
+++ todoapp/projects/tests.py
@@ -1,53 +1,137 @@
-from rest_framework.test import APITestCase
+
 from django.urls import reverse
 
-from users.models import CustomUser
-from projects.models import Project
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from projects import models as project_models
+from users import models as user_models
 
 
-class ProjectAPITest(APITestCase):
+class ProjectMemberAPITests(APITestCase):
 
     def setUp(self):
-        self.user1 = CustomUser.objects.create(email="user1@gmail.com", password="user@123",
-                                              first_name="John", last_name="Dio")
-        self.user2 = CustomUser.objects.create(email="user2@gmail.com", password="user@123",
-                                              first_name="John", last_name="Dio")
-        self.user3 = CustomUser.objects.create(email="user3@gmail.com", password="user@123",
-                                               first_name="John", last_name="Dio")
-        self.user4 = CustomUser.objects.create(email="user4@gmail.com", password="user@123",
-                                               first_name="John", last_name="Dio")
-        self.project1 = Project.objects.create(name="project1", max_members=3)
-        self.project2 = Project.objects.create(name="project2", max_members=3)
-        self.project3 = Project.objects.create(name="project3", max_members=3)
-        
-    
-    def test_add_member(self):
-        pk = self.project1.id
-        action = 'add'
+        self.user1 = user_models.CustomUser.objects.create(
+            email='test1@gmail.com',
+            first_name='test',
+            last_name='user',
+            password='somepassword'
+        )
+        self.user2 = user_models.CustomUser.objects.create(
+            email='test2@gmail.com',
+            first_name='test',
+            last_name='user',
+            password='somepassword'
+        )
+        self.user3 = user_models.CustomUser.objects.create(
+            email='test3@gmail.com',
+            first_name='test',
+            last_name='user',
+            password='somepassword'
+        )
+        self.project1 = project_models.Project.objects.create(
+            name="TestProject1", max_members=2
+        )
+        self.project2 = project_models.Project.objects.create(
+            name="TestProject2", max_members=3
+        )
+        self.project3 = project_models.Project.objects.create(
+            name="TestProject3", max_members=4
+        )
+        self.url_add = reverse(
+            'projects:projectmember', kwargs={'pk': self.project1.id, 'action': 'add'}
+        )
+        self.url_remove = reverse(
+            'projects:projectmember', kwargs={'pk': self.project1.id, 'action': 'remove'}
+        )
 
-        url = reverse('projects:projectmember', args=[pk, action])
+    def test_adding_members_to_project(self):
+        response = self.client.patch(
+            self.url_add,
+            {'user_ids': [self.user2.id, self.user3.id]},
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(project_models.ProjectMember.objects.filter(
+            project=self.project1, member=self.user2).exists())
+        self.assertTrue(project_models.ProjectMember.objects.filter(
+            project=self.project1, member=self.user3).exists())
+        self.assertEqual(
+            response.data['log'][self.user2.id], 'added successfully')
+        self.assertEqual(
+            response.data['log'][self.user3.id], 'added successfully')
 
-        data = {
-            "uids" : [self.user1.id]
-        }
-        
-        response = self.client.patch(url, data, HTTP_AUTHORISATION=f'Token {self.token.key}', format='json')
-        
-        self.assertEqual(response.status_code, self.HTTP_200_OK)
-    
-    def test_remove_member(self):
-        pk  = self.project1.id
-        action = 'remove'
+    def test_remove_users_from_project(self):
+        project_models.ProjectMember.objects.create(
+            project=self.project1, member=self.user1)
+        project_models.ProjectMember.objects.create(
+            project=self.project1, member=self.user2)
+        response = self.client.patch(
+            self.url_remove,
+            {'user_ids': [self.user1.id, self.user2.id]},
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertFalse(project_models.ProjectMember.objects.filter(
+            project=self.project1, member=self.user1).exists())
+        self.assertFalse(project_models.ProjectMember.objects.filter(
+            project=self.project1, member=self.user2).exists())
+        self.assertEqual(
+            response.data['log'][self.user1.id], 'deleted successfully')
+        self.assertEqual(
+            response.data['log'][self.user2.id], 'deleted successfully')
 
-        url = reverse('projects:projectmember', args=[pk, action])
+    def test_addding_existing_member(self):
+        project_models.ProjectMember.objects.create(
+            project=self.project1, member=self.user1)
+        response = self.client.patch(
+            self.url_add,
+            {'user_ids': [self.user1.id]},
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data['log'][self.user1.id], 'project_id already map to member_id')
 
-        data = {
-            'user_ids' : [self.user1.id]
-        }
+    def test_user_already_in_two_projects(self):
+        project_models.ProjectMember.objects.create(
+            project=self.project2, member=self.user1)
+        project_models.ProjectMember.objects.create(
+            project=self.project3, member=self.user1)
+        response = self.client.patch(
+            self.url_add,
+            {'user_ids': [self.user1.id]},
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertFalse(project_models.ProjectMember.objects.filter(
+            project=self.project1, member=self.user1).exists())
+        self.assertEqual(response.data['log'][self.user1.id],
+                         'User can\'t be part of more than two projects')
 
-        response = self.client.patch(url, data, HTTP_AUTHORISATION=f'Token {self.token.key}', format='json')
+    def test_cannot_add_members_project_full(self):
+        project_models.ProjectMember.objects.create(
+            project=self.project1, member=self.user1)
+        project_models.ProjectMember.objects.create(
+            project=self.project1, member=self.user2)
+        response = self.client.patch(
+            self.url_add,
+            {'user_ids': [self.user3.id]},
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertFalse(project_models.ProjectMember.objects.filter(
+            project=self.project1, member=self.user3).exists())
+        self.assertEqual(response.data['log'][self.user3.id],
+                         'Project member reach maximum limit')
 
-        self.assertEqual(response.status_code, self.HTTP_200_OK)
-        
-
+    def test_cannot_remove_non_member(self):
+        response = self.client.patch(
+            self.url_remove,
+            {'user_ids': [self.user1.id]},
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data['log'][self.user1.id], 'user is not a member')
     
